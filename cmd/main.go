a4c635223ecde990e3d174632d64c36724e6d03a
Andrei Sousa <sousandrei@gmail.com>
2024-11-05 14:07:51 +0100

    feat: add wav conversion

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"normalizer/internal/ffmpeg"
 	"normalizer/internal/normalizer"
 	"os"
 	"path/filepath"
@@ -26,6 +27,11 @@ func main() {
 				Name:      "analyze",
 				ArgsUsage: "[FILES]",
 				Action:    handleAnalyze,
+			},
+			{
+				Name:      "wav",
+				ArgsUsage: "[FILES]",
+				Action:    handleWav,
 			},
 		},
 	}
@@ -92,5 +98,30 @@ func handleAnalyze(c *cli.Context) error {
 		fmt.Printf("mean_volume: -%s\n", strconv.FormatFloat(stats.Mean_volume, 'f', -1, 64))
 		fmt.Printf(" max_volume: -%s\n\n", strconv.FormatFloat(stats.Max_volume, 'f', -1, 64))
 	}
+	return nil
+}
+
+func handleWav(c *cli.Context) error {
+	for _, inputFilePath := range c.Args().Slice() {
+		if _, err := os.Stat(inputFilePath); os.IsNotExist(err) {
+			return fmt.Errorf("file %s does not exist", inputFilePath)
+		}
+
+		fmt.Printf("converting %s to wav\n", inputFilePath)
+
+		fileName := strings.TrimSuffix(inputFilePath, filepath.Ext(inputFilePath))
+		outputFilePath := fileName + ".wav"
+
+		_, err := ffmpeg.Run(
+			ffmpeg.WithInput(inputFilePath),
+			ffmpeg.WithAudioCodec("pcm_s16le"),
+			ffmpeg.WithOverwrite(),
+			ffmpeg.WithOutput(outputFilePath),
+		)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
